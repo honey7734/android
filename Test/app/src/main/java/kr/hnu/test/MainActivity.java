eddba673ec1209aa78a53674cf82c1eb18d05ea6
gusdml7243 <gusdml7243@gmail.com>
2022-03-15 23:04:23 +0900

    과제1

--- Test/app/src/main/java/kr/hnu/test/MainActivity.java
+++ Test/app/src/main/java/kr/hnu/test/MainActivity.java
@@ -12,11 +12,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.textview);
+        setContentView(R.layout.activity_main);
     }
-    public void mOnClick(View v) {
+/*    public void mOnClick(View v) {
         EditText edit = (EditText) findViewById(R.id.edit);
         String str = edit.getText().toString();
+        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+    }
+*/
+    public void login(View v) {
+        EditText edit = (EditText) findViewById(R.id.inputID);
+        String str = "ID:"+ edit.getText().toString() + "\n";
+        edit = (EditText) findViewById(R.id.inputPw);
+        str += "password:" + edit.getText().toString();
         Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
     }
 }
