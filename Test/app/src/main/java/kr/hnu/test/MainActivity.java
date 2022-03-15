7ac27a2a228e5fb4cf769a5f5540ebc3a34e452f
gusdml7243 <gusdml7243@gmail.com>
2022-03-15 22:17:37 +0900

    뷰 연습

--- Test/app/src/main/java/kr/hnu/test/MainActivity.java
+++ Test/app/src/main/java/kr/hnu/test/MainActivity.java
@@ -3,12 +3,20 @@ package kr.hnu.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.textview);
+    }
+    public void mOnClick(View v) {
+        EditText edit = (EditText) findViewById(R.id.edit);
+        String str = edit.getText().toString();
+        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
     }
 }
