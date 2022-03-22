eff0244526c177416a3fd50de1962c5c52109784
honey7734 <gusdml7243@gmail.com>
2022-03-22 21:54:45 +0900

    과제2 로그인 화면에서 ID를 입력하면 다른 화면에서 입력한 ID가 버튼과 목록에 나오도록 하시오

--- Test/app/src/main/java/kr/hnu/test/MainActivity.java
+++ Test/app/src/main/java/kr/hnu/test/MainActivity.java
@@ -1,38 +1,25 @@
 package kr.hnu.test;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView mText;
-    ActivityResultLauncher<Intent> activityResultLauncher;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mText = (TextView) findViewById(R.id.text);
-        init();
     }
-    public void mOnClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnedit:
-                Intent intent = new Intent(this, SubActivity.class);
-                intent.putExtra("TextIn", mText.getText().toString());
-                activityResultLauncher.launch(intent);
-                break;
-        }
-    }
-    void init() {
-        activityResultLauncher = registerForActivityResult(new
-                ActivityResultContracts.StartActivityForResult(), result -> {
-            if (result.getResultCode() == RESULT_OK) {
-                mText.setText(result.getData().getStringExtra("TextOut"));
-            }
-        });
+
+    public void login(View v) {
+        EditText edit = (EditText) findViewById(R.id.inputID);
+        String str = "ID:"+ edit.getText().toString() + "\n";
+        edit = (EditText) findViewById(R.id.inputPw);
+        str += "password:" + edit.getText().toString();
+        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
     }
 }
--- Test/app/src/main/java/kr/hnu/test/SubActivity.java
+++ Test/app/src/main/java/kr/hnu/test/SubActivity.java
@@ -8,29 +8,5 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SubActivity extends AppCompatActivity {
-    EditText mEdit;
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
-        mEdit = (EditText)findViewById(R.id.stredit);
-        Intent intent = getIntent();
-        String text = intent.getStringExtra("TextIn");
-        if (text != null) {
-            mEdit.setText(text);
-        }
-    }
-    public void mOnClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnok:
-                Intent intent = new Intent();
-                intent.putExtra("TextOut", mEdit.getText().toString());
-                setResult(RESULT_OK,intent);
-                finish();
-                break;
-            case R.id.btncancel:
-                setResult(RESULT_CANCELED);
-                finish();
-                break;
-        }
-    }
+
 }
