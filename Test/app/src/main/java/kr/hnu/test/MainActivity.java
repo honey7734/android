e34bd432f516c4cdc378d60da145e45d53d6cd95
honey7734 <gusdml7243@gmail.com>
2022-03-23 10:43:59 +0900

    안드로이드 과제2 완료

--- Test/app/src/main/java/kr/hnu/test/MainActivity.java
+++ Test/app/src/main/java/kr/hnu/test/MainActivity.java
@@ -1,25 +1,50 @@
 package kr.hnu.test;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText eId;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eId = (EditText) findViewById(R.id.inputID);
+
     }
 
     public void login(View v) {
+        /*
+        //로그인 메세지 띄우기
         EditText edit = (EditText) findViewById(R.id.inputID);
         String str = "ID:"+ edit.getText().toString() + "\n";
         edit = (EditText) findViewById(R.id.inputPw);
         str += "password:" + edit.getText().toString();
         Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+       */
+
+        //화면 전환(이름 id전송)
+
+        Intent intent = new Intent(this, SubActivity.class);
+        //mId의 문자열을 TextIn이라는 이름으로 읽어와 인텐트에 저장
+        intent.putExtra("TextIn", eId.getText().toString());
+        startActivity(intent);
+
+
+
     }
+
 }
--- Test/app/src/main/java/kr/hnu/test/SubActivity.java
+++ Test/app/src/main/java/kr/hnu/test/SubActivity.java
@@ -1,12 +1,52 @@
 package kr.hnu.test;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SubActivity extends AppCompatActivity {
 
+    LinearLayout linear;
+    String name;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        linear = (LinearLayout) findViewById(R.id.linear);
+        Button button = linear.findViewById(R.id.btnnewmessage);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("TextIn");
+
+        if (name != null) {
+            button.setText(name+" 나와라");
+        }
+
+    }
+    public void mOnClick(View v) {
+        RelativeLayout rel = (RelativeLayout)View.inflate(this, R.layout.message, null);
+        ImageView img = (ImageView) rel.findViewById(R.id.messageImg);
+        TextView text = rel.findViewById(R.id.content);
+        if (linear.getChildCount() % 2 == 0) {
+            rel.setBackgroundColor(Color.GRAY);
+            img.setImageResource(R.drawable.ellie);
+            text.setText(name+2);
+
+        } else {
+            rel.setBackgroundColor(Color.RED);
+            img.setImageResource(R.drawable.nico);
+            text.setText(name+1);
+        }
+        linear.addView(rel);
+    }
 }
